04d19f77a815a47e8ae6c517ad0d9c6a61156b1d
ZHUYuting <yuting910520@gmail.com>
2014-06-10 10:12:07 +0200

    ajoute debug.c et error.c

---
+++ debug.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "debug.h"
+
+void print_help(){
+    printf("Available commands:");
+    printf("\t h \t help\n");
+    printf("\t c \t continue (exit interactive debug mode)\n");
+    printf("\t s \t step by step (next instruction)\n");
+    printf("\t RET \t step by step (next instruction)\n");
+    printf("\t r \t print registers\n");
+    printf("\t d \t print data memory\n");
+    printf("\t t \t print text (program) memory\n");
+    printf("\t p \t print text (program) memory\n");
+    printf("\t m \t print registers and data memory\n");
+}
+
+
+bool debug_ask(Machine *pmach){
+    printf("DEBUG? ");
+    char c=getchar();
+    switch (c) {
+    case 'h':
+        print_help();
+        break;
+    case 'c':
+        return false;
+    case 's':
+    case 'RET':
+        break;
+    case 'r':
+        print_cpu(pmach);
+        break;
+    case 'd':
+        print_data(pmach);
+        break;
+    case 't':
+    case 'p':
+        print_program(pmach);
+        break;
+    case 'm':
+        print_cpu(pmach);
+        print_data(pmach);
+        break;
+    default:
+        break;
+    }
+    return true;
+}
+
+int main(){
+    debug_ask(NULL);
+    return 0;
+}
---
+++ error.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+#include "error.h"
+
+#define MAX 100
+void warning(Warning warn, unsigned addr){
+    if(warn==WARN_HALT){
+        printf("WARNING: HALT reached at address 0x%x\n", addr);
+    }
+}
+
+void error(Error err, unsigned addr){
+    switch (err) {
+    case 0:
+        printf("ERROR: NO ERROR at address 0x%x\n", addr);
+        break;
+    case 1:
+        printf("ERROR: UNKNOWN INSTRUCTION at address 0x%x\n", addr);
+        break;
+    case 2:
+        printf("ERROR: ILLEGAL INSTRUCTION at address 0x%x\n", addr);
+        break;
+    case 3:
+        printf("ERROR: ILLEGAL CONDITION at address 0x%x\n", addr);
+        break;
+    case 4:
+        printf("ERROR: FORBIDDEN VALUE at address 0x%x\n", addr);
+        break;
+    case 5:
+        printf("ERROR: TEXT SEGMENT VIOLATION at address 0x%x\n", addr);
+        break;
+    case 6:
+        printf("ERROR: DATA SEGMENT VIOLATION at address 0x%x\n", addr);
+        break;
+    case 7:
+        printf("ERROR: STACK SEGMENT VIOLATION at address 0x%x\n", addr);
+        break;
+    default:
+        break;
+    }
+    exit(0);
+}
+
+int main(){
+    warning(WARN_HALT,0x30);
+    error(ERR_IMMEDIATE,0x05);
+}
