ce114c83508f276dbb3de00fbb0821c7ca8aaef7
MEURGUES Nicolas <nico@user-PC.(none)>
2014-06-10 09:58:21 +0200

    Modifications minimes de instruction.c

--- instruction.c
+++ instruction.c
@@ -1,18 +1,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-//! Forme imprimable des codes opérations
+//! Forme imprimable des codes operations
 const char* cop_names[] = { "ILLOP", "NOP", "LOAD", "STORE", "ADD", "SUB", "BRANCH", "CALL", "RET", "PUSH", "POP", "HALT" };
 
 
 //! Forme imprimable des conditions
 const char* condition_names[] = { "NC", "EQ", "NE", "GT", "GE", "LT", "LE" };
 
-void print_op(Instruction instr);
-
-//! Impression d'une instruction sous forme lisible (désassemblage)
+//! Impression d'une instruction sous forme lisible (desassemblage)
 /*!
- * \param instr l'instruction à imprimer
+ * \param instr l'instruction a imprimer
  * \param addr son adresse
  */
 void print_instruction(Instruction instr, unsigned addr) {
@@ -20,52 +18,52 @@ void print_instruction(Instruction instr, unsigned addr) {
     int reg = instr->instr_generic->_regcond;
 
     switch (instr.instr_generic._cop) {
-        //Pas d'accés mémoire :
+        //Pas d'acces memoire :
         case ILLOP:
         case NOP:
         case RET:
         case HALT:
             break;
 
-        //Accés mémoire :
+        //Acces memoire :
         case LOAD:
         case STORE:
         case ADD:
         case SUB:
             //affiche registre d'instr sous forme intelligible.
             printf("R%02d, ", reg);
-            //appel de print_op
-            print_op(instr);
+            //appel de print_operande
+            print_operande(instr);
             break;
 
         case BRANCH:
         case CALL:
             //affiche le code condition d'une instruction sous forme intelligible
             printf("%s ", condition_names[reg]);
-            //appel de print_op
-            print_op(instr);
+            //appel de print_operande
+            print_operande(instr);
             break;
 
-        //Accés à la pile :
+        //Acces a la pile :
         case PUSH:
         case POP:
-            //appel de print_op
-            print_op(instr);
+            //appel de print_operande
+            print_operande(instr);
             break;
     }
 }
 
-//affiche les opérandes des opérations d'une instruction inst sous forme intelligible.
-void print_op(Instruction instr) {
+//affiche les operandes des operations d'une instruction inst sous forme intelligible.
+void print_operande(Instruction instr) {
     bool immediate = instr->instr_generic->_immediate; // immediate = I
     bool indexed = instr->instr_generic->_indexed; // indexed = X
 
     if (immediate) {
-        // si immediate = 1, affiche la valeur imédiate
+        // si immediate = 1, affiche la valeur imediate
         printf("#%d", instr->instr_immediate->_value);
     } else {                
         if (indexed) {
-            // Si immediate = 0 et indexed = 1 nous sommes dans le cas d'un adressage indexé
+            // Si immediate = 0 et indexed = 1 nous sommes dans le cas d'un adressage indexe
             printf("%+d[", (int) instr->instr_indexed->_offset); // Offset sous la forme +/-offset
             printf("R%02d]", (int) instr->instr_indexed->_rindex); // Registre pour l'adressage indirect [R..]
         } else {
