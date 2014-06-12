11d89fbdfd51c86274779e0a25ca47f91bef4ee3
Fares <tochirou.aelys@gmail.com>
2014-06-12 21:03:56 +0200

    correction pop

--- exec.c
+++ exec.c
@@ -222,7 +222,7 @@ void refresh_condition(Machine *pmach, unsigned int regcond) {
 	unsigned ad_Data;
 
 	check_not_immediate(instr, addr);
-	check_stack(pmach, pmach->_sp++, addr); // on incrémente sp et verifie qu'on ne sort pas de la pile
+	check_stack(pmach, ++pmach->_sp, addr); // on incrémente sp et verifie qu'on ne sort pas de la pile
 	ad_Data = get_adress(pmach, instr);
 	check_overflow(pmach, ad_Data, addr);
 	check_overflow(pmach, pmach->_sp, addr);
