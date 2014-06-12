970863a960f2be15ef1714aa74981f26e52be3ca
Fares <tochirou.aelys@gmail.com>
2014-06-12 20:30:59 +0200

    modification du fichier test

--- Examples/test2.asm
+++ Examples/test2.asm
@@ -1,23 +1,30 @@
-// Copie d'un tableau dans un autre :
+// Copie d'éléments d'un endroit à un autre
 
         TEXT    30
         EQU     *
-        LOAD    R01, @13
-        LOAD    R02, @14
-
-        LOAD    R03, +4[R01]
-        STORE   R03, +4[R02]
-        LOAD    R03, +4[R01]
-        STORE   R03, +4[R02]
-        LOAD    R03, +4[R01]
-        STORE   R03, +4[R02]
+        LOAD    R01, #0
+        LOAD    R02, #4
 
         LOAD    R03, +0[R01]
         STORE   R03, +0[R02]
+
+        LOAD    R03, +1[R01]
+        STORE   R03, +1[R02]
+
+        LOAD    R03, +2[R01]
+        STORE   R03, +2[R02]
+
+        LOAD    R03, +3[R01]
+        STORE   R03, +3[R02]
+
+        HALT
         END
 
         DATA    30
-        WORD    0
+        elem1    WORD    1
+        elem2    WORD    2
+        elem3    WORD    3
+        elem4    WORD    4
 
         END
         
